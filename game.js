7ca996c0b47207250eb284f5faf97afd87604963
Brandon Rockhold <brandon.rockhold@cerner.com>
2015-09-26 09:59:37 -0500

    Committing logic to get enemies moving randomly around the screen, as well as for when they die (despite the fact that they can't actually die yet)...

--- game.js
+++ game.js
@@ -244,38 +244,98 @@ function outOfBounds(a, x, y) {
 }
 
 function Enemy() {
+    // src coords of Enemy image in sprite file
     this.srcX = 140;
     this.srcY = 600;
+
+    // dimension of Enemy image in sprite
     this.width = 45;
     this.height = 54;
 
+    // coords for the location of the Enemy
     this.drawX = randomRange(0, canvasWidth - this.width);
     this.drawY = randomRange(0, canvasHeight - this.height);
 
+    // coords for the center of the Enemy in its current location
     this.centerX = this.drawX + (this.width / 2);
     this.cernerY = this.drawY + (this.height / 2);
 
-    //this.targetX = this.centerX;
-    //this.targetY = this.centerY;
+    // target coords for where the Enemy will move next
+    this.targetX = this.centerX;
+    this.targetY = this.centerY;
 
-    //this.randomMoveTime = randomRange(4000, 10000);
+    // time interval between enemy movements
+    this.randomMoveTime = randomRange(4000, 10000);
+
     this.speed = 1;
 
-    //var that = this;
-    //this.moveInterval = setInterval(function() {this.setTargetLocation();}, that.randomMoveTime);
+    var that = this;
+    this.moveInterval = setInterval(function() {
+        that.setTargetLocation();
+    }, that.randomMoveTime);
 
     this.isDead = false;
 }
 
 Enemy.prototype.update = function() {
-    //this.checkDirection();
     this.centerX = this.drawX + (this.width / 2);
     this.centerY = this.drawY + (this.height / 2);
+    this.checkDirection();
 };
 
 Enemy.prototype.draw = function() {
     ctxEntities.drawImage(imgSprite, this.srcX, this.srcY, this.width, this.height, this.drawX, this.drawY, this.width,
         this.height);
+};
+
+Enemy.prototype.setTargetLocation = function() {
+    this.randomMoveTime = randomRange(4000, 10000);
+    var minX = this.centerX - 50;
+    var maxX = this.centerX + 50;
+    var minY = this.centerY - 50;
+    var maxY = this.centerY + 50;
+
+    if (minX < 0) {
+        minX = 0;
+    }
+
+    if (maxX > canvasWidth) {
+        maxX = canvasWidth;
+    }
+
+    if (minY < 0) {
+        minY = 0;
+    }
+
+    if (maxY > canvasHeight) {
+        maxY = canvasHeight;
+    }
+
+    this.targetX = randomRange(minX, maxX);
+    this.targetY = randomRange(minY, maxY);
+};
+
+Enemy.prototype.checkDirection = function() {
+    if (this.centerX < this.targetX) {
+        this.drawX += this.speed;
+    } else if (this.centerX > this.targetX) {
+        this.drawX -= this.speed;
+    }
+
+    if (this.centerY < this.targetY) {
+        this.drawY += this.speed;
+    } else if (this.cetnerY > this.targetY) {
+        this.drawY -= this.speed;
+    }
+};
+
+Enemy.prototype.die = function() {
+    var soundEffect = new Audio("audio/dying.wav");
+    soundEffect.play();
+    clearInterval(this.moveInterval);
+
+    this.srcX = 185;
+    this.isDead = true;
 };
 
 function initEnemies() {
